590735193612a2a978ed17a03f8eb846c721ef6e
Styrbæk.com <mail@styrbaek.com>
2026-02-18 14:44:04 +0100

    Add 400x400 ad size support and ignore backend public app directory

--- backend/config/smartads.php
+++ backend/config/smartads.php
@@ -4,6 +4,7 @@ return [
     'cron_queue_token' => env('CRON_QUEUE_TOKEN', ''),
 
     'allowed_ad_sizes' => [
+        ['width' => 400, 'height' => 400],
         ['width' => 800, 'height' => 800],
         ['width' => 1080, 'height' => 1080],
         ['width' => 1080, 'height' => 1350],
